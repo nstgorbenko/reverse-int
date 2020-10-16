10ed97e62f95f14282cb41e33038216feedddcd7
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-10-16 23:48:48 +0300

    feat: implement reverse digits function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    const stringNumber = String(Math.abs(n));
+    let reverseString = '';
   
+    for (let i = stringNumber.length - 1; i >= 0; i--) {
+      reverseString += stringNumber[i];
+    }
+  
+    return Number(reverseString);
 }
